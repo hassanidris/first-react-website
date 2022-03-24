5273a4974673cd00dca7f9007d3e1d564078e5ac
hassanidris <hassan.creative@gmail.com>
2022-03-24 16:40:21 +0300

    Fixing banner

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => (
     <p className='p__roboto' style={{ letterSpacing: '0.2rem', color: '#ec351a', textTransform: 'uppercase', fontSize: '1rem'}}>Hos oss är du i fokus!</p>
     <HeadingOnWhite titleP1='Välkommen till ' titleP2='Miljonbemanning' />
     {/* <h1 className='mb__header-h1'>Välkommen till <span style={{color: '#ec351a', fontWeight: '700'}}>Miljonbemanning</span></h1> */}
-    <p className='p__opensans' style={{width: '80%'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+    <p className='p__opensans' style={{width: '100%'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
     
     <div className='flex__center'>
       <CustomButton title='connect' />
